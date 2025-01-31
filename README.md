2f768bcef0760f5187cbd8c8c5bdee8eb051c4ab
matheusssilva991 <matheus.ssilva991@gmail.com>
2025-01-31 13:28:18 -0300

    feat: add readme global

---
+++ README.md
@@ -0,0 +1,27 @@
+# Tarefa de Temporização U4C5 - Embarcatech
+
+Este repositório contém dois projetos práticos utilizando temporização (timers) para controlar LEDs. O foco é demonstrar como usar a biblioteca hardware/timer.h da Raspberry Pi Pico SDK para criar ciclos temporizados, controlando LEDs e botões com diferentes comportamentos.
+
+## Tarefas
+
+1. [Semáforo com Temporização](https://github.com/matheusssilva991/tarefas_temporizador_U4C5/tree/main/tarefa1_temporizador_U4C5)
+2. [Botão com Temporização](https://github.com/matheusssilva991/tarefas_temporizador_U4C5/tree/main/tarefa2_temporizador_U4C5)
+
+## Como executar
+
+Para executar os projetos, siga as instruções de cada tarefa. Cada tarefa possui um arquivo README.md com as instruções necessárias para compilar e executar o projeto.
+
+## 🤝 Equipe
+
+Membros da equipe de desenvolvimento do projeto:
+<table>
+  <tr>
+    <td align="center">
+      <a href="https://github.com/matheusssilva991">
+        <img src="https://github.com/matheusssilva991.png" width="100px;" alt="Foto de Matheus Santos Silva no GitHub"/><br>
+        <b>Matheus Santos Silva (matheusssilva991)</b>
+        <p>Desenvolvedor Back-end - NestJS</p>
+      </a>
+    </td>
+  <tr>
+</table>
--- tarefa1_temporizador_U4C5/tarefa1_temporizador_U4C5.c
+++ tarefa1_temporizador_U4C5/tarefa1_temporizador_U4C5.c
@@ -3,12 +3,13 @@
 #include "pico/stdlib.h"
 #include "hardware/timer.h"
 
+// Pinos dos leds
 #define RED_LED_PIN 11
 #define YELLOW_LED_PIN 12
 #define GREEN_LED_PIN 13
 
-const int LEDS_DELAY = 3000;
-static volatile int led_id = 1;
+const int LEDS_DELAY = 3000; // Delay entre os leds
+static volatile int led_id = 1; // Identificador do led
 
 bool repeating_timer_callback(repeating_timer_t *rt);
 void init_led(uint led_pin);
@@ -17,41 +18,51 @@ int main()
 {
     stdio_init_all();
 
+    // Inicializa os leds
     init_led(RED_LED_PIN);
     init_led(YELLOW_LED_PIN);
     init_led(GREEN_LED_PIN);
 
+    // Inicializa o temporizador
     struct repeating_timer timer;
 
     add_repeating_timer_ms(LEDS_DELAY, repeating_timer_callback, NULL, &timer);
 
     while (true)
     {
+        // Tempo absoluto em ms
         uint32_t start_ms = to_ms_since_boot(get_absolute_time());
 
+        // Aguarda 1 segundo
         sleep_ms(1000);
 
+        // Imprime o tempo absoluto em ms
         printf("Tempo absoluto em ms: %d \n", start_ms);
     }
 }
 
+// Função de callback do temporizador
 bool repeating_timer_callback(repeating_timer_t *rt)
 {
+    // Alterna entre os leds
     switch (led_id)
     {
     case 1:
+        // Led Vermelho
         printf("Led Vermelho.\n");
         gpio_put(RED_LED_PIN, 1);
         gpio_put(YELLOW_LED_PIN, 0);
         gpio_put(GREEN_LED_PIN, 0);
         break;
     case 2:
+        // Led Amarelo
         printf("Led Amarelo.\n");
         gpio_put(RED_LED_PIN, 0);
         gpio_put(YELLOW_LED_PIN, 1);
         gpio_put(GREEN_LED_PIN, 0);
         break;
     case 3:
+        // Led Verde
         printf("Led Verde.\n");
         gpio_put(RED_LED_PIN, 0);
         gpio_put(YELLOW_LED_PIN, 0);
@@ -59,11 +70,13 @@ bool repeating_timer_callback(repeating_timer_t *rt)
         break;
     }
 
+    // Altera o led
     led_id = (led_id % 3) + 1;
 
     return true;
 }
 
+// Inicializa o pino do led
 void init_led(uint led_pin)
 {
     gpio_init(led_pin);
